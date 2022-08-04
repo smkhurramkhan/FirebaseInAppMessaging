a87ee140c180ba6aca49da2920ed2930eb864d1e
S M Khurram Khan <smkhurramkhan@gmail.com>
2022-08-04 13:06:00 +0500

    first commit

---
+++ app/src/main/java/com/example/firebaseinappmessaging/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.firebaseinappmessaging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.FirebaseApp
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("TAG", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = token
+            Log.d("TAG", msg)
+           // Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+
+
+                // to get token to send message to a test device
+            //search this Starting InAppMessaging runtime with Installation ID in Info TAB
+            // get that id and add it in firebase
+        })
+
+    }
+}
